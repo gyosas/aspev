06bb97fd6411fdbadb21a0cf48b7c525f06e7b5c
José Oliveira <gyosas@Joses-MBP.lan>
2023-04-18 17:58:49 +0100

    First commit of this project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { ApiService } from './pages/services/api.service';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  templateUrl: 'app.component.html',
 })
 export class AppComponent {
-  title = 'aspev-app';
+  public someData: any[];
+
+  constructor(private apiService: ApiService) {}
+
+  public ngOnInit(): void {
+    this.apiService.getSomeData().subscribe((data) => {
+      this.someData = data;
+    });
+  }
 }
